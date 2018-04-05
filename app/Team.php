b61aee7576d1dc1c0bbd532e228f7cc7d9b52c45
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-04-05 07:29:41 -0600

    Update fillables in Team model

--- app/Team.php
+++ app/Team.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
+    protected $fillable = ['name'];
+
     public function players() {
         return $this->hasMany('App\Player');
     }
