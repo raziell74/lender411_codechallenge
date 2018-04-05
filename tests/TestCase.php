b53229fd6f5b182a674321a0b015f68e94d1cfb8
Jordan Richmeier <raziell74@users.noreply.github.com>
2018-04-05 02:52:59 -0600

    Database Setup (#1)
    * Create team and player tables
    
    * Add api_key to users table
    
    * Add users seed
    
    * Update user seeder to use factory
    
    * Fix seeder call
    
    * Add sql restraints for player and team uniqueness
    
    * update seeder for users to use create
    
    * Use uuid for the api_key
    
    * Fix time stamp columns so mysql manages them
    
    * Add Team table model, factory, and seed
    
    * Add players model and wire it to teams
    
    Updates both team and player models and the teams seeder
    
    * Setup a seperate testing db with setup and teardown
    
    * Import missing artisan class
    
    * Add player model unit tests
    
    Player model also needed an update for the "team" relation
    
    * Allow players to not belong to a team
    
    * Test Team model

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+    }
+
+    public function tearDown()
+    {
+        Artisan::call('migrate:reset');
+        parent::tearDown();
+    }
 }
