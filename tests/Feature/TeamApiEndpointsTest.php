228d442517a603470f10190291872e385bc5663b
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-04-05 09:42:54 -0600

    Include get and getAll unit tests for teams api endpoint

--- tests/Feature/TeamApiEndpointsTest.php
+++ tests/Feature/TeamApiEndpointsTest.php
@@ -10,11 +10,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class TeamApiEndpointsTest extends TestCase
 {
     public function testTeamsAreCreatedCorrectly() {
-        $user = Passport::actingAs(
-            factory(\App\User::class)->create(),
-            ['add-teams']
-        );
-        $token = $user->accessToken;
+        $token = $this->getOAuthToken();
         $headers = ['Authorization' => "Bearer $token"];
         $payload = [
             'name' => 'test name',
@@ -56,17 +52,53 @@ class TeamApiEndpointsTest extends TestCase
             'name' => 'Shablagoo'
         ]);
         $team->players()->saveMany(factory('App\Player', 4)->make());
-        $user = Passport::actingAs(
-            factory(\App\User::class)->create(),
-            ['delete-teams']
-        );
-        $token = $user->accessToken;
+        $token = $this->getOAuthToken();
         $headers = ['Authorization' => "Bearer $token"];
 
         $response = $this->json('DELETE', "/api/teams/{$team->id}", $headers);
-        $teamShablagoo = \App\Team::find($team->id);
+        $deleted_team = \App\Team::find($team->id);
 
         $response->assertStatus(204);
-        $this->assertNull($teamShablagoo);
+        $this->assertNull($deleted_team);
+    }
+
+    public function testGetTeam() {
+        $team = factory(\App\Team::class)->create(['name' => 'Shablagoo']);
+        $team->players()->saveMany(factory('App\Player', 4)->make());
+        $token = $this->getOAuthToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $response = $this->json('GET', "/api/teams/{$team->id}", $headers);
+        $output = json_decode($response->content(), true);
+
+        $response->assertStatus(200);
+        $this->assertEquals($output['name'], 'Shablagoo');
+        $this->assertCount(4, $output['players']);
+    }
+
+    public function testGetAllTeams() {
+        factory(\App\Team::class, 10)->create()->each(function($team) {
+            $team->players()->saveMany(factory('App\Player', 4)->make());
+        });
+        $token = $this->getOAuthToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $response = $this->json('GET', "/api/teams", $headers);
+        $output = json_decode($response->content(), true);
+
+        $response->assertStatus(200);
+        $this->assertCount(10, $output);
+        $this->assertCount(4, $output[0]['players']);
+    }
+
+    private function getOAuthToken() {
+        return Passport::actingAs(
+            factory(\App\User::class)->create(),
+            [
+                'add-teams',
+                'delete-teams',
+                'view-teams'
+            ]
+        )->accessToken;
     }
 }
